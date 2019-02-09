35ce069d81b4e7c4e659f3a48e8b05ba20e76720
Jarmo Riikonen <amatrelan@gmail.com>
2019-02-09 10:51:42 +0200

    added more test and other features, live CI

--- Twister/DynamicScraper.py
+++ Twister/DynamicScraper.py
@@ -1,55 +1,74 @@
 #!/usr/bin/env python3
 from shutil import which
+
 from selenium import webdriver
+from selenium.common.exceptions import InvalidArgumentException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException, InvalidArgumentException
-from selenium.webdriver.firefox.options import Options
 
 
 class DynamicScraper:
+    """This module creates a scraper what can handle some dynamic websites."""
+
     driver = None
     link = None
-
     options = Options()
     options.headless = True
-
     delay = 3
 
     def __init__(self):
+        """Initialize DynamicScraper class and creates sametime new driver."""
         self.driver = self.GetDriver()
 
     def GetDriver(self):
-        if (which("geckodriver") is not None):
+        """Check what dirivers user has installed on computer and tries use them."""
+        if which("geckodriver") is not None:
             print("Initialized Geckodriver")
-            driver = webdriver.Firefox(options=self.options)
+            driver = webdriver.Firefox(
+                options=self.options, service_log_path="/dev/null"
+            )
 
-        elif (which("chromedriver") is not None):
+        elif which("chromedriver") is not None:
             print("Initialized Chromedriver")
             driver = webdriver.Chrome(options=self.options)
 
         else:
-            print("You need to have geckodriver or "
-                  "chromedriver installed and in PATH to use this."
-                  "And if you use chromedriver, you need chrome installed,"
-                  "and same if you use geckodriver, you need firefox")
-            quit()
+            print(
+                "You need to have geckodriver or "
+                "chromedriver installed and in PATH to use this."
+                "And if you use chromedriver, you need chrome installed,"
+                "and same if you use geckodriver, you need firefox"
+            )
+            return None
 
         return driver
 
     def FindVideoSrc(self, link):
+        """Try to find video src attribute from link what is provided.
+
+        Args
+        link: Is link to website that contain dynamic video element
+
+        Returns
+        Link what's in video element. Or 'None' if fails to find one.
+
+        """
         try:
             self.driver.get(link)
             print("Trying to find video")
-            if (link.find("twist") > 0):
+            if link.find("twist") > 0:
                 element = WebDriverWait(self.driver, self.delay).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR,
-                                                    "video[src*='anime']")))
+                    EC.presence_of_element_located(
+                        (By.CSS_SELECTOR, "video[src*='anime']")
+                    )
+                )
 
             else:
                 element = WebDriverWait(self.driver, self.delay).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, "video")))
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "video"))
+                )
 
             temp = element.get_attribute("src")
             self.driver.close()
@@ -57,4 +76,5 @@ class DynamicScraper:
 
         except (TimeoutException, AttributeError, InvalidArgumentException):
             self.driver.close()
-            print("Page loading was too slow")
+            print("Page loading failed")
+            return None
--- Twister/test_DynamicScraper.py
+++ Twister/test_DynamicScraper.py
@@ -1,19 +1,22 @@
 from Twister.DynamicScraper import DynamicScraper
 
-test_src = 'https://twist.moe/anime/18if/[HorribleSubs]%2018if%20-%2001%20[1080p].mp4'
+test_src = "https://twist.moe/anime/18if/[HorribleSubs]%2018if%20-%2001%20[1080p].mp4"
 
 
 def test_create_and_close_driver():
+    """Creates new driver and closes it."""
     testDriver = DynamicScraper()
     testDriver.driver.close()
 
 
 def test_find_video_src_at_twist_moe():
+    """Creates new driver and tries to find video source."""
     testDriver = DynamicScraper()
 
     assert testDriver.FindVideoSrc("https://twist.moe/a/18if/1") == test_src
 
 
 def test_find_video_src_fail():
+    """Creates new driver and tries to find src with bad link."""
     testDriver = DynamicScraper()
-    testDriver.FindVideoSrc("asd")
+    assert testDriver.FindVideoSrc("asd") is None
